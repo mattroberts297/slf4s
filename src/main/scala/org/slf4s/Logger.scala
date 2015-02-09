23f14f2d825c8a4117ae9e10d01c9bdfadaa45cf
Matt Roberts <mattroberts297@gmail.com>
2015-02-09 21:30:23 +0000

    Replaced case class with class and object.

--- src/main/scala/org/slf4s/Logger.scala
+++ src/main/scala/org/slf4s/Logger.scala
@@ -1,16 +1,20 @@
 package org.slf4s
 
-import org.slf4j.{Logger => JLogger}
+import org.slf4j.{Logger => Underlying}
 
-case class Logger(private val log: JLogger) {
-  def trace(msg: => String): Unit = if (log.isTraceEnabled) log.trace(msg)
-  def trace(msg: => String, t: => Throwable): Unit = if (log.isTraceEnabled) log.trace(msg, t)
-  def debug(msg: => String): Unit = if (log.isDebugEnabled) log.debug(msg)
-  def debug(msg: => String, t: => Throwable): Unit = if (log.isDebugEnabled) log.debug(msg, t)
-  def info(msg: => String): Unit = if (log.isInfoEnabled) log.info(msg)
-  def info(msg: => String, t: => Throwable): Unit = if (log.isInfoEnabled) log.info(msg, t)
-  def warn(msg: => String): Unit = if (log.isWarnEnabled) log.warn(msg)
-  def warn(msg: => String, t: => Throwable): Unit = if (log.isWarnEnabled) log.warn(msg, t)
-  def error(msg: => String): Unit = if (log.isErrorEnabled) log.error(msg)
-  def error(msg: => String, t: => Throwable): Unit = if (log.isErrorEnabled) log.error(msg, t)
+class Logger(val underlying: Underlying) {
+  def trace(msg: => String): Unit = if (underlying.isTraceEnabled) underlying.trace(msg)
+  def trace(msg: => String, t: => Throwable): Unit = if (underlying.isTraceEnabled) underlying.trace(msg, t)
+  def debug(msg: => String): Unit = if (underlying.isDebugEnabled) underlying.debug(msg)
+  def debug(msg: => String, t: => Throwable): Unit = if (underlying.isDebugEnabled) underlying.debug(msg, t)
+  def info(msg: => String): Unit = if (underlying.isInfoEnabled) underlying.info(msg)
+  def info(msg: => String, t: => Throwable): Unit = if (underlying.isInfoEnabled) underlying.info(msg, t)
+  def warn(msg: => String): Unit = if (underlying.isWarnEnabled) underlying.warn(msg)
+  def warn(msg: => String, t: => Throwable): Unit = if (underlying.isWarnEnabled) underlying.warn(msg, t)
+  def error(msg: => String): Unit = if (underlying.isErrorEnabled) underlying.error(msg)
+  def error(msg: => String, t: => Throwable): Unit = if (underlying.isErrorEnabled) underlying.error(msg, t)
+}
+
+object Logger {
+  def apply(underlying: Underlying): Logger = new Logger(underlying)
 }
