895d577ba3f38f42762e4cac961cc174a3cf510d
Matt Roberts <mattroberts297@gmail.com>
2015-02-25 06:36:19 +0000

    Added new javadoc. Updated home page.

---
+++ api/1.7.10/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"org" : [], "org.slf4s" : [{"object" : "org\/slf4s\/Logger$.html", "class" : "org\/slf4s\/Logger.html", "name" : "org.slf4s.Logger"}, {"object" : "org\/slf4s\/LoggerFactory$.html", "name" : "org.slf4s.LoggerFactory"}, {"trait" : "org\/slf4s\/Logging.html", "name" : "org.slf4s.Logging"}]};
